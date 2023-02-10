1b2380215a73a69e61074f7675bcd76fd072ed2c
Valefar1989 <103574729+Valefar1989@users.noreply.github.com>
2023-02-10 22:25:09 +0300

    dz3

---
+++ OOPjava/Seminar3/Main.java
@@ -0,0 +1,25 @@
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DoublyLinkedList list = new DoublyLinkedList();
+        list.add("data1");
+        list.add("data2");
+
+        list.add(1, "data2_1");
+
+
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()){
+            String element = iterator.next();
+            System.out.println(element);
+        }
+
+    }
+}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
